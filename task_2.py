9d5c4e61b8afa5908703552b8da6d65de3ebdf14
hundredwishes <115923657+hundredwishes@users.noreply.github.com>
2022-10-27 00:34:29 +0200

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,1 +1,23 @@
-# Remove this comment to confirm that this task is done
+from random import randint
+from turtle import exitonclick, forward, left, penup, pendown, back
+
+inflacja = 15
+
+def rectangle(a,b):
+    for _ in range(2):
+        forward(a)
+        left(90)
+        forward(b)
+        left(90)
+
+penup()
+back(100)
+pendown()
+for _ in range(20):
+    rectangle(15,inflacja)
+    penup()
+    forward(20)
+    pendown()
+    inflacja += randint(-10,30)
+
+exitonclick()
