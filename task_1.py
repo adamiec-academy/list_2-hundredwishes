eb593672d7550949d4bd04d9932275f83e942677
hundredwishes <115923657+hundredwishes@users.noreply.github.com>
2022-10-27 00:33:45 +0200

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,2 +1,11 @@
-def remove_parentheses(text):
-    return text
+def remove_parentheses(t):
+    result=""
+    is_inside= False
+    for letter in t:
+        if letter == "(":
+            is_inside = True
+        elif letter == ")":
+            is_inside = False
+        elif is_inside == False:
+            result += letter
+    return result.strip()
