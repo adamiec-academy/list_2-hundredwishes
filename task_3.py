3e62f442d1ddd03b49caeb6735d6f812d93eac3d
hundredwishes <115923657+hundredwishes@users.noreply.github.com>
2022-10-31 10:13:09 +0100

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,6 +1,24 @@
 def is_prime(n):
-    return
-
+    number = True
+    if n == 1:
+        number = False
+        return number
+    else:
+        for i in range (2,n):
+            if (n%i==0):
+                number = False
+        return number
 
 def is_diabolic(n):
-    return
+    devil = False
+    if "666" in str(n):
+        devil = True
+    return devil
+
+for i in range(1,100000):
+    ile = 0
+    if is_prime(i)==True: 
+        if is_diabolic(i)==True:
+            print(i)
+            ile += 1
+print("Takich liczb jest:", ile)
