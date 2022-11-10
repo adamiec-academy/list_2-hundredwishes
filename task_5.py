419b94cdcfa1f14d71ff5dad2e8b9a7aee588fae
hundredwishes <115923657+hundredwishes@users.noreply.github.com>
2022-11-10 14:39:28 +0100

    Update task_5.py

--- task_5.py
+++ task_5.py
@@ -1,6 +1,27 @@
 def cipher(text, shift):
-    return text
-
+    result=""
+    for sign in text:
+        if sign == " ":
+            result += sign
+        else:
+            number = ord(sign) + shift
+            if (number in range(65,91) or number in range(97,123)):
+                result += chr(number)
+            else:
+                number -=26
+                result += chr(number)    
+    return result
 
 def decipher(text, shift):
-    return text
+    result=""
+    for sign in text:
+        if sign == " ":
+            result += sign
+        else:
+            number = ord(sign) - shift
+            if ((number in range(65,91) and sign.isupper()==True) or (number in range(97,123) and sign.isupper()==False)):
+                result += chr(number)
+            else:
+                number +=26
+                result += chr(number)    
+    return result
